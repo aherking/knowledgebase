62b1c7cba966654b52913761ab54f34985d099cc
Andre Herking <a.herking@shopware.com>
2019-05-28 11:10:25 +0000

    Implemented article overview with different main tag sortings

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Tag;
 use App\Form\ArticleType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,9 +23,13 @@ class ArticleController extends AbstractController
         $articles = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findAll();
+        $tags = $this->getDoctrine()
+            ->getRepository((Tag::class))
+            ->findAll();
 
         return $this->render('article/index.html.twig', [
             'articles' => $articles,
+            'tags' => $tags
         ]);
     }
 
